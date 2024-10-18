8c1081095cb192a87d27b80209684e860944af1d
s1lent18 <alijafar.rajani@gmail.com>
2024-10-18 17:41:18 +0500

    Facebook Login Added, Normal DB Login Added Account Page Adjusted

--- app/src/main/java/com/example/mezbaan/Mezbaan.kt
+++ app/src/main/java/com/example/mezbaan/Mezbaan.kt
@@ -1,9 +1,7 @@
 package com.example.mezbaan
 
 import android.app.Application
-import android.content.res.Configuration
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
 class Mezbaan : Application()
--- app/src/main/java/com/example/mezbaan/ui/theme/Type.kt
+++ app/src/main/java/com/example/mezbaan/ui/theme/Type.kt
@@ -1,7 +1,6 @@
 package com.example.mezbaan.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
